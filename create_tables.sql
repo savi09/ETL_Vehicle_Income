71f9a63d394b55983a3b3ef12886909426e009ff
Susana Villagrana <savi@ucdavis.edu>
2021-09-22 22:07:08 -0700

    Change to SQL file

--- create_tables.sql
+++ create_tables.sql
@@ -2,6 +2,7 @@
 CREATE TABLE Zip_Vehicle_Info (
 ID SERIAL PRIMARY KEY,
 Date DATE,
+Year INT,
 Zip_Code INT,
 Model_Year VARCHAR(50),
 Fuel_Type VARCHAR(50),
@@ -10,8 +11,9 @@ Duty TEXT,
 Count_Vehicles VARCHAR(50)
 );
 
-CREATE TABLE Zip_Avg_Info (
+CREATE TABLE Zip_Avg_Income (
 ID SERIAL PRIMARY KEY,
+Year INT,
 State TEXT,
 Zip_Code INT,
 Total_Pop INT,
@@ -20,8 +22,9 @@ Country TEXT,
 Avg_Income INT
 );
 
-CREATE TABLE Zip_Avg_Income (
+CREATE TABLE Zip_Vehicle_Income (
 ID SERIAL PRIMARY KEY,
+Year INT,
 Zip_Code INT,
 Count_Vehicles VARCHAR(50),
 Avg_Income INT,
